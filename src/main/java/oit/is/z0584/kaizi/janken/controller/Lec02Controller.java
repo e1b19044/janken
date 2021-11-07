13abfdef46257f4b9f8a8aee7e533e9f77b5333a
e1b19044 <e1b19044@oit.ac.jp>
2021-11-08 03:50:50 +0900

    DIを用いてエントリー中のユーザーを表示する処理の実装

--- src/main/java/oit/is/z0584/kaizi/janken/controller/Lec02Controller.java
+++ src/main/java/oit/is/z0584/kaizi/janken/controller/Lec02Controller.java
@@ -1,5 +1,8 @@
 package oit.is.z0584.kaizi.janken.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,13 +10,27 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import oit.is.z0584.kaizi.janken.model.Entry;
 
 @Controller
 @RequestMapping("/lec02")
 public class Lec02Controller {
 
+  @Autowired
+  private Entry entry;
+
+    /**
+   *
+   * @param model Thymeleafにわたすデータを保持するオブジェクト
+   * @param prin  ログインユーザ情報が保持されるオブジェクト
+   * @return
+   */
   @GetMapping
-  public String lec02(){
+  public String lec03(ModelMap model, Principal prin){
+    String loginUser = prin.getName(); // ログインユーザ情報
+    this.entry.addUser(loginUser);
+    model.addAttribute("login_user", loginUser);
+    model.addAttribute("entry", this.entry);
     return "lec02.html";
   }
 
--- src/main/java/oit/is/z0584/kaizi/janken/security/Lec03AuthConfiguration.java
+++ src/main/java/oit/is/z0584/kaizi/janken/security/Lec03AuthConfiguration.java
@@ -37,21 +37,13 @@ public class Lec03AuthConfiguration extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     // Spring Securityのフォームを利用してログインを行う
     http.formLogin();
-    // http://localhost:8000/sample3 で始まるURLへのアクセスはログインが必要
+    // http://localhost:8080/lec02 で始まるURLへのアクセスはログインが必要
     // antMatchers().authenticated がantMatchersへのアクセスに認証を行うことを示す
     // antMatchers()の他にanyRequest()と書くとあらゆるアクセス先を表現できる
     // authenticated()の代わりにpermitAll()と書くと認証処理が不要であることを示す
     http.authorizeRequests().antMatchers("/lec02/**").authenticated();
 
-    // Spring Securityの機能を利用してログアウト．ログアウト時は http://localhost:8000/ に戻る
+    // Spring Securityの機能を利用してログアウト．ログアウト時は http://localhost:8080/ に戻る
     http.logout().logoutSuccessUrl("/");
-
-    /**
-     * 以下2行はh2-consoleを利用するための設定なので，開発が完了したらコメントアウトすることが望ましい
-     * CSRFがONになっているとフォームが対応していないためアクセスできない
-     * HTTPヘッダのX-Frame-OptionsがDENYになるとiframeでlocalhostでのアプリが使えなくなるので，H2DBのWebクライアントのためだけにdisableにする必要がある
-     */
-    http.csrf().disable();
-    http.headers().frameOptions().disable();
   }
 }
